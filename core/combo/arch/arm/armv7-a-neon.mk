ca72ea7e212cc084abe0f68936e90597798193a5
Ron Asimi <ron.asimi@gmail.com>
2014-06-29 15:33:09 -0400

    JustArchi's ArchiDroid Optimizations V3
    Change-Id: Ife98f13869e29d09b7fae56e30da2c743a95a99d

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -2,6 +2,7 @@
 # Generating binaries for the ARMv7-a architecture and higher with NEON
 #
 ARCH_ARM_HAVE_ARMV7A            := true
+ARCH_ARM_HAVE_TLS_REGISTER      := true
 ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
@@ -9,11 +10,12 @@ ARCH_ARM_HAVE_NEON              := true
 ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
 	arch_variant_cflags := -mcpu=cortex-a15
 else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
+ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a9)
 	arch_variant_cflags := -mcpu=cortex-a9
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
 	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_ldflags := -Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7
